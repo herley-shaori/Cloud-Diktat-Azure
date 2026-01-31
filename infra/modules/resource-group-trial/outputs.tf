328e1fd5b9c6993ff716125a5434ea6a03335596
herley <herleys@xlsmart.co.id>
2026-01-31 22:40:25 +0700

    Add resource-group-trial module

---
+++ infra/modules/resource-group-trial/outputs.tf
@@ -0,0 +1,4 @@
+output "resource_group_id" {
+  description = "ID of the trial resource group"
+  value       = azurerm_resource_group.trial.id
+}
---
+++ infra/modules/resource-group-trial/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the trial resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region for the trial resource group"
+}
