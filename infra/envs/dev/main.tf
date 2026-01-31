beb73a354fbf0a8538a6bf6e9eae8139721db0d0
herley <herleys@xlsmart.co.id>
2026-01-31 22:48:42 +0700

    Use resource-group-trial module in dev

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -2,3 +2,10 @@ output "hello_world" {
   description = "Simple sanity output"
   value       = "hello world"
 }
+
+module "resource_group_trial" {
+  source = "../../modules/resource-group-trial"
+
+  resource_group_name = "rg-trial-dev"
+  location            = "eastus"
+}
